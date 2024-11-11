fd09b5ce8d5de18f10f86b4dbec2aeae521dda24
furkan icoz <f_icoz@hotmail.com>
2024-11-11 21:13:57 +0300

    add custom view modifier for badge

---
+++ MovieList/MovieList/Common/CustomViewModifiers/CustomBagdeModifier.swift
@@ -0,0 +1,30 @@
+
+struct CustomBagdeModifier: ViewModifier {
+    
+    let count: Int
+    
+    func body(content: Content) -> some View {
+        if count > 0 {
+            ZStack(alignment: .topTrailing) {
+                content
+                Text(count, format: .number)
+                    .font(.system(size: 14))
+                    .foregroundStyle(Color.white)
+                    .padding(.horizontal, 5)
+                    .padding(.vertical, 5)
+                    .background(Color.red)
+                    .frame(height: 17)
+                    .clipShape(Capsule())
+                    .offset(x: 5, y: -5)
+            }
+        } else {
+            content
+        }
+    }
+}
+
+extension View {
+    func badge(count: Int) -> some View {
+        modifier(CustomBagdeModifier(count: count))
+    }
+}
