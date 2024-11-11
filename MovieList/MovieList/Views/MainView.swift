c833fbe3be0005c7254b307d49945c4c00e5eab1
furkan icoz <f_icoz@hotmail.com>
2024-11-11 20:02:37 +0300

    Create Xcode project

---
+++ MovieList/MovieList/Views/MainView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  MovieList
+//
+//  Created by Furkan ic on 10.11.2024.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    
+    var body: some View {
+        Text("Hello Swift")
+    }
+}
+
+#Preview {
+    MainView()
+}
---
+++ MovieList/MovieListTests/MovieListTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieListTests.swift
+//  MovieListTests
+//
+//  Created by Furkan ic on 10.11.2024.
+//
+
+import Testing
+@testable import MovieList
+
+struct MovieListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
